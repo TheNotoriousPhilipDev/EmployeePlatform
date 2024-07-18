84efe8f984cc03cc99ccf2da4875c11008a08016
andres gomez <afgomez0622@gmail.com>
2024-07-18 15:46:14 -0500

    Project structure

---
+++ EmployeManagementBack/src/test/java/com/agomez/backendapp/employemanagementback/EmployeManagementBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agomez.backendapp.employemanagementback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeManagementBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
